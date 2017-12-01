ccfb51a7be54938be00ce63d27df625794dc30e3
nehrr <iamkapitaan@gmail.com>
2017-12-01 12:38:03 +0100

    complete Home component retrieving json data and randomizing them

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Caps from '../Caps';
+import DataJSON from '../../caps.json';
 
-const Home = () => (
-  <span>Random cap of the moment</span>
-  <Caps />
-)
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = { caps: [] };
+  }
+
+  _RandomCaps() {
+    let randomCaps = DataJSON[Math.floor(Math.random() * DataJSON.length)];
+    this.setState ({ caps: randomCaps})
+    console.log(randomCaps);
+  }
+
+  componentWillMount() {
+    this._RandomCaps();
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={this.state.caps.pathcaps} />
+        <h4>{this.state.caps.movie}</h4>
+        <a>Share action</a>
+        <a>Favourite action</a>
+        <a>Like action</a>
+      </div>
+    )
+  }
+}
 
 export default Home;
