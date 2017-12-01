f4b3ff22039eb14a4db803c43f8a1ccd818e3b7f
nehrr <iamkapitaan@gmail.com>
2017-12-01 11:38:01 +0100

    changed structure of caps

--- src/components/Caps/index.js
+++ src/components/Caps/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 class Caps extends React.Component {
+
   render() {
     return (
     <div>
-      <img src='address' />
-      <h4>Movie Title</h4>
-      <a>Like action</a>
+      <img src='{address}' />
+      <h4>{Movie Title}</h4>
       <a>Share action</a>
       <a>Favourite action</a>
+      <a>Like action</a>
     </div>
   )
   }
