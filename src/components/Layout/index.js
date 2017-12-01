fac59ccf1be61ec7f6c114b7f3ffbb14444f7785
nehrr <iamkapitaan@gmail.com>
2017-12-01 17:23:42 +0100

    beautified with Bulma

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import logo from './Capszinga_logo.png';
 import Caps from '../Caps'
 
 class Layout extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Caps</h1>
-          <div className="App">
-              <Link to='/signin'>SignIn</Link>
-              &nbsp; &nbsp;
-              <Link to='/signup'>SignUp</Link>
-              &nbsp; &nbsp;
-              <Link to='/'>Home</Link>
-              &nbsp; &nbsp;
-              <Link to='/gallery'>Gallery</Link>
-              &nbsp; &nbsp;
-              <Link to='/upload'>Upload</Link>
-              &nbsp; &nbsp;
-              <Link to='/profile'>Profile</Link>
+      <div>
+        <section class="hero is-light is-medium">
+
+          <div class="hero-head">
+            <nav className="level">
+              <p className="level-item has-text-centered">
+                <a className="link is-info"><Link to='/signin'>Sign In</Link></a>
+              </p>
+              <p className="level-item has-text-centered">
+                <a className="link is-info"><Link to='/signup'>Sign Up</Link></a>
+              </p>
+              <p className="level-item has-text-centered">
+                <a className="link is-info"><Link to='/'>Home</Link></a>
+              </p>
+
+              <p className="level-item has-text-centered">
+                <img src={logo} alt="logo" />
+              </p>
+              <p className="level-item has-text-centered">
+                <a className="link is-info"><Link to='/gallery'>Gallery</Link></a>
+              </p>
+              <p className="level-item has-text-centered">
+                <a className="link is-info"><Link to='/upload'>Upload</Link></a>
+              </p>
+              <p className="level-item has-text-centered">
+                <a className="link is-info"><Link to='/profile'>Profile</Link></a>
+              </p>
+            </nav>
           </div>
-        </header>
+        </section>
 
-          {this.props.children}
+        <section class="hero is-dark is-fullheight">
+          <div class="hero-body">
+            <div class="container has-text-centered">
+              {this.props.children}
+            </div>
+          </div>
 
+        </section>
       </div>
     );
   }
