721eaadd0640d1dc9fda172482a23d34adc12bdd
nehrr <iamkapitaan@gmail.com>
2017-12-06 12:11:17 +0100

    did a placeholder version of sign up and upload

--- src/components/SignUP/index.js
+++ src/components/SignUP/index.js
@@ -1,7 +1,77 @@
 import React from 'react';
 
 const SignUp = () => (
-  <div>Hello</div>
+  <section class="hero is-fullheight">
+
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <div class="column is-4 is-offset-4">
+        <h3 class="title has-text-grey">Sign up</h3>
+        <div class="box">
+
+          <form id="sign-up" method="POST" action="" enctype="application/json">
+
+            <div class="field">
+              <label class="label has-text-left">Username</label>
+              <div class="control">
+                <input name="username" class="input is-large" type="text" placeholder="Username" autofocus="" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label has-text-left">Last name</label>
+              <div class="control">
+                <input name="lastname" class="input is-large" type="text" placeholder="Last name" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label has-text-left">First name</label>
+              <div class="control">
+                <input name="firstname" class="input is-large" type="text" placeholder="First name" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label has-text-left">Birthdate</label>
+              <div class="control">
+                <input name="birthdate" class="input is-large" type="date" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label has-text-left">Email</label>
+              <div class="control">
+                <input name="email" class="input is-large" type="email" placeholder="your@email.com" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label has-text-left">Password</label>
+              <div class="control">
+                <input name="password" class="input is-large" type="password" placeholder="Password" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label has-text-left">Password Confirmation</label>
+              <div class="control">
+                <input name="password_confirmation" class="input is-large" type="password" placeholder="Password" />
+              </div>
+            </div>
+
+            <a class="button is-block is-info is-large is-success" onclick="">
+              Sign up
+            </a>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 );
 
 export default SignUp;
--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
 const Upload = () => (
-  <span>Upload</span>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-4 is-offset-4">
+          <h3 class="title has-text-grey">Upload</h3>
+          <div class="box">
+
+            <form id="upload" method="POST" action="">
+
+              <div class="file has-name is-boxed is-centered">
+                <label class="file-label">
+                  <input class="file-input" type="file" name="file" />
+                  <span class="file-cta">
+                    <span class="file-icon">
+                      <i class="fa fa-upload"></i>
+                    </span>
+                    <span class="file-label">
+                      Choose a file…
+                    </span>
+                  </span>
+                  <span class="file-name">
+                    {/* ${dynamic file name} */}
+                  </span>
+                </label>
+              </div>
+
+              <br />
+              <a class="button is-block is-info is-large" onClick="">Send file</a>
+            </form>
+          </div>
+          <p class="has-text-grey">
+            <a href="/index.html">*please submit form to proceed*</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 )
 
 export default Upload;
