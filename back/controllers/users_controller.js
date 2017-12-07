b6e073dba40e93ebb26890645864dae67cac98ec
nehrr <iamkapitaan@gmail.com>
2017-12-07 12:28:11 +0100

    changed architecture to reflect creation and use of API

---
+++ back/controllers/users_controller.js
@@ -0,0 +1,26 @@
+const db = require('../models');
+
+class UsersController {
+
+  // GET /users
+  index(req, res) {
+
+  }
+
+  // GET /users/:id
+  show(req, res) {
+
+  }
+
+  // PUT /users/:id
+  update(req, res) {
+
+  }
+
+  // DELETE /users/:id
+  delete(req, res) {
+
+  }
+}
+
+module.exports = new UsersController();
