ffb07b398fd0f468966297054d2e82de3074e88c
nehrr <iamkapitaan@gmail.com>
2017-12-08 17:45:01 +0100

    handling random caps with a refresh button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,39 +1,54 @@
 import React from 'react';
 import Caps from '../Caps';
 import Axios from 'axios';
-import _ from 'lodash';
 // import DataJSON from '../../caps.json';
 
 class Home extends React.Component {
   constructor() {
     super();
-    this.state = {caps: [] };
+    this.state = {caps: null};
+    // this._fetchRandom();
+    this.refresh = this.refresh.bind(this);
   }
 
-  _fetchData() {
-    Axios.get('/screencaps/')
+  _fetchRandom() {
+
+    Axios.get('/screencaps/random/')
     .then((res) => {
-      console.log(res);
-      this.setState({caps: res.data.caps});
-    })
-  }
 
-  _getCaps() {
-    return this.state.caps.map( aCap => {
-      return (
-        <Caps picture={aCap.path} title={aCap.movie.title} key={aCap.id}/>
-      )
+      this.setState({caps: res.data.caps});
+
     })
   };
 
+  refresh() {
+    this._fetchRandom();
+  };
+
+  _getCap() {
+    if(this.state.caps) {
+    // console.log(this.state);
+      return (
+        <Caps picture={this.state.caps.path} title={this.state.caps.movie.title} key={this.state.id}/>
+      )
+    } else {
+      return (
+        <div>Loading...</div>
+      )
+    }
+  };
+
   componentDidMount() {
-    this._fetchData();
+    this._fetchRandom();
   }
 
   render() {
+
     return (
       <div>
-        {_.sample(this._getCaps())}
+        <span onClick={this.refresh} className='button is-warning'>Refresh</span>
+        <br /> <br />
+        {this._getCap()}
       </div>
     )
   }
